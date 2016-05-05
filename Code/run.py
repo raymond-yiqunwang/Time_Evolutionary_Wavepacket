55663f5c1c460484f7a36697e5b8522ec8db36bb
Raymond <raymond931118@gmail.com>
2016-05-05 18:56:10 +0800

    2 commit

--- Code/run.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("python rand.py")
-os.system("mpif90 -I ~/Documents/program/Fortran/Library/ ~/Documents/program/Fortran/Library/*.o /usr/local/lib/libfftw3.a  main.f90")
-os.system("./a.out")
